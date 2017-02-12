61b0e28eb53300db74cd242e5dc3453d528fa3a8
YuchengZhao <crazyyucheng@gmail.com>
2017-02-12 14:54:41 +0800

    新增我的页面信息后台数据绑定

--- pages/my/my.js
+++ pages/my/my.js
@@ -1,8 +1,13 @@
 // pages/my/my.js
 var app = getApp();
+var Bmob = require('../../utils/bmob.js');
 Page({
   data:{
-    userInfo: {}
+    userInfo: {},
+    stu_id: '',
+    stu_name:'',
+    stu_class:'',
+    fenshu:''
   },
   onLoad:function(options){
     var that = this
@@ -13,6 +18,29 @@ Page({
         userInfo:userInfo
       })
     })
+    var value = wx.getStorageSync('stu_id')
+    this.setData({
+      stu_id : value
+    })
+    var Student = Bmob.Object.extend("Student");
+    var query = new Bmob.Query(Student);
+    var stuId = this.data.stu_id
+    query.equalTo("Student_Id", stuId);
+    // 查询所有数据
+    query.find({
+        success: function(results) {
+            var object = results[0];
+                that.setData({
+                  stu_name : object.get('Student_Name'),
+                  stu_class : object.get('Student_Class'),
+                  fenshu : object.get('Grade_TotalScore')
+                })
+        },
+        error: function(error) {
+            console.log("查询失败: " + error.code + " " + error.message);
+        }
+    });
+
   },
   onReady:function(){
     // 页面渲染完成
